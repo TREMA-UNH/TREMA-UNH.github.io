5e89e2ac5f07f64f8d2e4cb08962d67e76ebb72a
Ben Gamari <ben@smart-cactus.org>
2022-02-21 23:55:33 -0500

    Add citeproc support

--- hakyll.hs
+++ hakyll.hs
@@ -17,9 +17,30 @@ main = hakyll $ do
 
         match "content/**.mkd" $ do
                 route   $ setExtension "html" `composeRoutes` gsubRoute "content/" (const "")
-                compile $ pandocCompiler
+                compile $ pandocMathCompiler
                         >>= loadAndApplyTemplate "templates/default.html" defaultContext
                         >>= relativizeUrls
 
         match "templates/*" $ compile templateCompiler
+
+pandocMathCompiler :: Compiler (Item String)
+pandocMathCompiler =
+    let mathExtensions = extensionsFromList
+            [ Ext_tex_math_dollars
+            , Ext_tex_math_double_backslash
+            , Ext_latex_macros
+            ]
+        defaultExtensions = writerExtensions defaultHakyllWriterOptions
+        newExtensions = pandocExtensions <> mathExtensions <> extensionsFromList [Ext_citations]
+        writerOptions = defaultHakyllWriterOptions {
+                          writerExtensions = newExtensions,
+                          writerHTMLMathMethod = MathJax ""
+                        }
+        readerOptions = defaultHakyllReaderOptions {
+                          readerExtensions = newExtensions
+                        }
+        doCitations doc = unsafeCompiler $ do doc' <- handle (\(SomeException e) -> print e >> throw e) $ processCites' doc
+                                              print doc'
+                                              return doc'
+    in pandocCompilerWithTransformM readerOptions writerOptions doCitations
 
